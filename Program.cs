2bb50c91ed71b401d1185a4336c904120ead6d40
Anatolii <zavorinanatolii@mail.ru>
2022-09-21 14:03:28 +0800

    функция

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string[] Main(string[] array)
+{
+  string[] result = new string[0];
+  for (int i = 0; i < array.Length; i++)
+  {
+    string  correct = array[i];
+    int j = result.Length;
+    if (correct.Length <= 3)
+    {
+      Array.Resize(ref result, result.Length +1);
+      result[j] = array[i];
+    }
+  }
+  return result;
+}
+
